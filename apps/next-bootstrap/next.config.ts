69b5d1c7af1217e4d922333e96317ae94a049564
Maksym Bondarchuk <belkamax05@gmail.com>
2025-05-25 06:18:35 +0100

    init

--- apps/next-bootstrap/next.config.ts
+++ apps/next-bootstrap/next.config.ts
@@ -1,12 +1,9 @@
-//@ts-check
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { composePlugins, withNx } = require('@nx/next');
+import { NextConfig } from 'next';
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
-const nextConfig = {
+const nextConfig:NextConfig = {
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
   nx: {},
@@ -17,9 +14,9 @@ const nextConfig = {
   },
 };
 
-const plugins = [
-  // Add more Next.js plugins to this list if needed.
-  withNx,
-];
+// const plugins = [
+//   // Add more Next.js plugins to this list if needed.
+//   withNx,
+// ];
 
-module.exports = composePlugins(...plugins)(nextConfig);
+// module.exports = composePlugins(...plugins)(nextConfig);
